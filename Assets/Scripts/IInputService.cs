1a4e515fd0c024a6272cf00e59485a38e01ae7ae
datngo <datkurrosenpai@gmail.com>
2026-01-23 10:22:34 +0700

    Input system new....

--- Assets/Scripts/IInputService.cs
+++ Assets/Scripts/IInputService.cs
@@ -4,20 +4,6 @@ public interface IInputService
 {
     Vector2 GetMoveInput();
     bool IsJumpPressed();
-    bool IsAttackPressed(); // <--- Thêm dòng này
-}
-
-public class UnityInputService : IInputService
-{
-    public Vector2 GetMoveInput()
-    {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-        return new Vector2(x, z);
-    }
-
-    public bool IsJumpPressed() => Input.GetButtonDown("Jump");
-
-    // Mặc định Fire1 là Chuột trái hoặc Chạm màn hình
-    public bool IsAttackPressed() => Input.GetButtonDown("Fire1");
+    bool IsAttackPressed();
+    void Cleanup(); // Nhớ dòng này để gọi được hàm dọn dẹp
 }
---
+++ Assets/Scripts/UnityInputService.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class UnityInputService : IInputService
+{
+    // Class MyControl do Unity tự sinh ra (không có s)
+    private MyControl _controls;
+
+    public UnityInputService()
+    {
+        _controls = new MyControl();
+        _controls.Enable(); // Bật Input lên
+    }
+
+    // Hàm dọn dẹp (Quan trọng cho Android)
+    public void Cleanup()
+    {
+        _controls.Disable();
+    }
+
+    public Vector2 GetMoveInput()
+    {
+        // Đọc từ New Input System
+        return _controls.Player.Move.ReadValue<Vector2>();
+    }
+
+    public bool IsJumpPressed()
+    {
+        return _controls.Player.Jump.WasPressedThisFrame();
+    }
+
+    public bool IsAttackPressed()
+    {
+        return _controls.Player.Attack.WasPressedThisFrame();
+    }
+}
